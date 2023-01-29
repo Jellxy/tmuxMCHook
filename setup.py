803f5638f34d283fb4fcf08e20d13022928d8f76
Jellxy <88067520+Jellxy@users.noreply.github.com>
2023-01-29 15:34:44 +1300

    setup file for install via pip

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'tmuxMCHook',
+    version = '0.1.1',
+    description = 'Manipulate Minecraft servers with Python code.',
+    author = 'Jellxy',
+    author_email = 'jellxyr@gmail.com',
+    url = 'https://github.com/Jellxy/tmuxMCHook'
+)
